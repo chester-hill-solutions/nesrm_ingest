833c22bcab8878e39453be454fd92e69b86cda21
sai <saihaansyed2003@gmail.com>
2025-06-02 05:26:46 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# NESRM Ingest
+
+This is the lambda function to handle NES Relationship Manager Ingestion.
+
+## Deploy
+
+```bash
+zip -r deploy.js .
+```
+
+Upload from > .zip file > Upload > Navigate to `deploy.zip` > Save
---
+++ index.mjs
@@ -0,0 +1,68 @@
+import { createClient } from "@supabase/supabase-js";
+
+function validatePayload(payload) {
+  const errors = [];
+
+  if (!payload.event.body.source_url) {
+    errors.push("Missing source_url");
+  }
+
+  return errors;
+}
+
+export const handler = async (event) => {
+  //check empty
+  if (!event) {
+    let response = {
+      statusCode: 400,
+      body: JSON.stringify("Missing payload"),
+    };
+    return response;
+  }
+
+  //connect to supabase
+  const supabase = createClient(process.env.DATABASE_URL, process.env.KEY);
+
+  //parse event
+  let payload = {};
+  if (typeof event === "string") {
+    console.log("Event is a string");
+    payload = JSON.parse(event);
+  } else {
+    console.log("Event is not a string");
+    payload = event;
+  }
+  console.log("Event: ", payload);
+
+  //validate
+  const validationErrors = validatePayload(payload);
+  if (validationErrors.length > 0) {
+    let response = {
+      statusCode: 422,
+      body: JSON.stringify(validationErrors),
+    };
+    return response;
+  }
+
+  //store request in supabase
+  const { data, requestStorageError } = await supabase
+    .from("request")
+    .insert({ payload: payload, source_url: payload.event.body.source_url });
+
+  if (requestStorageError) {
+    let response = {
+      statusCode: 400,
+      body: JSON.stringify({ errorType: "supabase", ...requestStorageError }),
+    };
+    return response;
+  }
+
+  console.log(data, requestStorageError);
+
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify("ahfbskbf"),
+  };
+
+  return response;
+};
